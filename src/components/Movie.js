7356dd011e01a959b633026e026439f2c03d06d8
Sam O'Connell <sjoconnell@Sams-MacBook-Pro.local>
2017-06-23 18:52:50 -0500

    added fonts and completed main page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -17,12 +17,19 @@ const Movie = ({film}) => {
   return (
     <div className="movie">
       <img className="movie-image" src={require(`../css/images/${film.title}.jpeg`)} alt={film.title}/>
-      <h1 className="movie-title">{film.title}</h1>
-      <h2 className="movie-director">{film.director}</h2>
+      <div className="movie-info">
+        <h1 className="movie-info-title">{film.title}</h1>
+        <h2 className="movie-info-director">{film.director}</h2>
+      </div>
       <div className="movie-characters">
         {
           getCharacters(film.episode_id)
-            .map(character => <img className="movie-characters-image" src={require(`../css/images/${character}.png`)} alt={character} key={character} />)
+            .map(character =>
+              <div className="movie-characters-container" key={character}>
+                <img className="movie-characters-image" src={require(`../css/images/${character}.png`)} alt={character}  />
+                <div className="movie-characters-container-hover">{character}</div>
+              </div>
+            )
         }
       </div>
     </div>
