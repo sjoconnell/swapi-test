1bec50e21e8676f9c60eaca82175ee9b2dccd845
Sam O'Connell <sjoconnell@Sams-MacBook-Pro.local>
2017-06-26 19:23:26 -0500

    added chart and removed victory

--- src/components/ScrawlChart.js
+++ src/components/ScrawlChart.js
@@ -4,15 +4,22 @@ import { VictoryBar, VictoryChart } from 'victory';
 const ScrawlChart = ({chartData = []}) => {
   return (
     <div className="scrawlChart">
-    { 
-      <VictoryChart>
-        <VictoryBar
-          data={chartData}
-          x="title"
-          y="wordCount"
-        />
-      </VictoryChart>
-    }
+      <p className="scrawlChart-title">Opening Scrawl Lengths</p>
+      <ul className="bar-graph">
+        <li className="bar-graph-axis">
+          <div className="bar-graph-label">100</div>
+          <div className="bar-graph-label">50</div>
+          <div className="bar-graph-label">0</div>
+        </li>
+        {
+          chartData.map(chart => 
+            <li className="bar alert" style={{height: `${chart.wordCount}%`}} key={chart.title}>
+              <div className="percent">{chart.wordCount}</div>
+              <div className="description">{chart.title}</div>
+            </li>
+          )
+        }
+      </ul>
     </div>
   )
 }
